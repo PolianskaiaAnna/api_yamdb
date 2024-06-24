992f33302dd6860619afa95932853df90df2107d
Павел Степанов <pavel.ntg@gmail.com>
2024-06-24 06:04:13 +0500

    Написал вьюсет для приозведений

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import CategoryViewSet, GenreViewSet
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 router_vers1 = routers.DefaultRouter()
 
@@ -10,6 +10,9 @@ router_vers1.register(
 )
 router_vers1.register(
     r'genres', GenreViewSet, basename='genres'
+)
+router_vers1.register(
+    r'titles', TitleViewSet, basename='titles'
 )
 
 urlpatterns = [
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-
 from rest_framework import viewsets
 
-from reviews.models import Category, Genre
-from api.serializers import CategorySerializer, GenreSerializer
+from reviews.models import Category, Genre, Title
+from api.serializers import (CategorySerializer,
+                             GenreSerializer,
+                             TitleReadSerializer,
+                             TitleWriteSerializer)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -18,3 +19,14 @@ class GenreViewSet(viewsets.ModelViewSet):
 
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Вьюсет для произведений."""
+
+    queryset = Title.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleReadSerializer
+        return TitleWriteSerializer
