d9cb3b8f956e7fa30caa20b9e242a61ec9df70f4
Павел Степанов <pavel.ntg@gmail.com>
2024-06-24 14:47:09 +0500

    Добавил фильтры и поправил произведения

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,11 +2,9 @@ from django.urls import include, path
 from rest_framework import routers
 
 from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
-#from users.views import UsersViewSet, SignUpView, TokenView, UserProfileAPIView
 
 router_vers1 = routers.DefaultRouter()
 
-#router_vers1.register(r'v1/users', UsersViewSet, basename='users')
 router_vers1.register(
     r'categories', CategoryViewSet, basename='categories'
 )
@@ -19,8 +17,4 @@ router_vers1.register(
 
 urlpatterns = [
     path('v1/', include(router_vers1.urls)),
-#    path('v1/users/me/', UserProfileAPIView.as_view(), name='profile'),
-#    path('', include(router_vers1.urls)),
-#    path('v1/auth/signup/', SignUpView.as_view(), name='signup'),
-#   path('v1/auth/token/', TokenView.as_view(), name='check_token'),
 ]
