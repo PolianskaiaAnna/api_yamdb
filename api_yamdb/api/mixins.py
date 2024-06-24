5c4ec3a9b27471371c88e2f8a75db8c3113959f9
Павел Степанов <pavel.ntg@gmail.com>
2024-06-24 13:08:07 +0500

    Проходят все тесты для категорий и жанров

---
+++ api_yamdb/api/mixins.py
@@ -0,0 +1,18 @@
+from rest_framework import filters, mixins, viewsets
+from rest_framework.pagination import PageNumberPagination
+
+from users.permissions import IsOwnerOrReadOnly
+
+
+class CreateListDestroyViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
+    """Миксин для Category/Genre ViewSet."""
+    permission_classes = (IsOwnerOrReadOnly,)
+    filter_backends = [filters.SearchFilter]
+    pagination_class = PageNumberPagination
+    search_fields = ['name']
+    lookup_field = 'slug'
