0e19de4a6a8834227148e4fe94f72f4d3072e1e2
Павел Степанов <pavel.ntg@gmail.com>
2024-06-29 17:24:16 +0500

    Импорт csv одной функцией

--- api_yamdb/reviews/management/commands/importcsv.py
+++ api_yamdb/reviews/management/commands/importcsv.py
@@ -1,30 +1,44 @@
-from django.core.management import BaseCommand, CommandError
-from django.db.utils import IntegrityError
 
-from ._importcsv import import_csv
+import csv
+
+from django.core.management.base import BaseCommand
+
+from api_yamdb.settings import Path
+from reviews.models import Category, Comment, Genre, Review, Title, User
 
 
 class Command(BaseCommand):
-    """Импортер данных из csv."""
-
     help = 'Импорт данных csv из /static/data/ в базу данных.'
 
     def handle(self, *args, **kwargs):
-        try:
-            import_csv()
-        except IntegrityError:
-            raise CommandError(
-                'Очистите базу данных перед загрузкой файлов csv,'
-                ' воспользуйтесь менеджмент командой flush')
-        except FileNotFoundError:
-            raise CommandError(
-                'Не найдены файлы csv в папке static/data')
-        except Exception:
-            raise CommandError(
-                'Непредвиденная ошибка при выполнении команды importcsv,'
-                ' обратитесь к разработчикам'
-            )
+        CSV_DIR = Path('static', 'data')
+        FILE_HANDLE = (
+            ('category.csv', Category, {}),
+            ('genre.csv', Genre, {}),
+            ('users.csv', User, {}),
+            ('titles.csv', Title, {'category': 'category_id'}),
+            ('genre_title.csv', Title.genre.through, {}),
+            ('review.csv', Review, {'author': 'author_id'}),
+            ('comments.csv', Comment, {'author': 'author_id'}),
+        )
+        for file, model, replace in FILE_HANDLE:
+            self.stdout.write(f'Начинаем импорт из файла {file}')
+            with open(Path(CSV_DIR, file), mode='r', encoding='utf8') as f:
+                reader = csv.DictReader(f)
+                counter = 0
+                objects_to_create = []
+                for row in reader:
+                    counter += 1
+                    args = dict(**row)
+                    if replace:
+                        for old, new in replace.items():
+                            args[new] = args.pop(old)
+                    objects_to_create.append(model(**args))
+                model.objects.bulk_create(objects_to_create,
+                                          ignore_conflicts=True)
+                self.stdout.write(
+                    f'Данные из файла {file} импортированы')
 
         self.stdout.write(self.style.SUCCESS(
-            'Все данные из csv файлов загружены в базу данных'
-        ))
+            'Все данные загружены из csv файлов загружены в базу')
+        )
